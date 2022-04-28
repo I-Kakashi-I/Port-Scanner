3a40e2290179e79851be734927787a2308dab185
Mohamed Mounir <mohamed.muonir2244@outlook.com>
2022-04-28 21:42:13 +0200

    Update Port_Scanner.py

--- Port_Scanner.py
+++ Port_Scanner.py
@@ -12,7 +12,7 @@ def scan_port(ipaddress, port):
 	try:
 		sock = socket.socket()
 		sock.connect((ipaddress, port))
-		print(termcolor.colored("[+] Port Opened " + str(port)))
+		print(termcolor.colored("[+] Port Opened " + str(port)), 'green')
 		sock.close()
 	except:
 		pass
@@ -21,7 +21,7 @@ def scan_port(ipaddress, port):
 targets = input("[*] Enter Targets To Scan(split them by -): ")
 ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
 if '-' in targets:
-	print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
+	print(termcolor.colored(("[*] Scanning Multiple Targets"), 'red'))
 	for ip_addr in targets.split('-'):
 		scan(ip_addr.strip(' '), ports)
 else:
